90fc02e7c15beb7e163aad3039f84bf1162dc7fe
Tim Savage <tim.savage@poweredbypenguins.org>
2013-11-26 02:06:56 +1100

    Added Pint Float field and test cases

--- src/odin/contrib/pint/fields.py
+++ src/odin/contrib/pint/fields.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from pint.unit import DimensionalityError
+import six
 from odin import exceptions
-from odin.contrib.pint import units
+from odin.contrib.pint.units import registry
 from odin.fields import Field
 from odin.validators import EMPTY_VALUES
-
 
 __all__ = ('FloatQField',)
 
@@ -11,36 +12,37 @@ __all__ = ('FloatQField',)
 class FloatQField(Field):
     default_error_messages = {
         'invalid': "'%s' value must be a float.",
-        'invalid-unit': "'%s' value must be a %s quantity.",
     }
 
-    def __init__(self, units_, **kwargs):
+    def __init__(self, units, **kwargs):
         super(FloatQField, self).__init__(**kwargs)
 
         # Ensure we have valid units
-        if units_ is None:
+        if units is None:
             raise ValueError("Units cannot be None")
+        if isinstance(units, six.string_types):
+            units = registry[units]
         #if not units_ in units.registry:
         #    raise ValueError("Units object is not a member of `odin.units.registry`. Any custom units must be "
         #                     "registered with Odin's unit registry.")
-        self.units = units_
+        self.units = units
 
     def to_python(self, value):
         if value in EMPTY_VALUES:
             return None
-        if isinstance(value, tuple):
-            pass
-        if isinstance(value, units.registry.Quantity):
-            if value.units != self.units:
-                msg = self.error_messages['invalid-unit'] % (value, self.units.units)
+        if not isinstance(value, registry.Quantity):
+            if isinstance(value, tuple) and len(value) == 2:
+                magnitude, units = value
+            else:
+                magnitude, units = value, self.units
+            if isinstance(units, six.string_types):
+                units = registry[units]
+            try:
+                value = float(magnitude) * units
+            except ValueError:
+                msg = self.error_messages['invalid'] % value
                 raise exceptions.ValidationError(msg)
-            return value
-
         try:
-            return float(value)
-        except ValueError:
-            msg = self.error_messages['invalid'] % value
-            raise exceptions.ValidationError(msg)
-
-    def to_string(self, value):
-        pass
+            return value.to(self.units)
+        except DimensionalityError as de:
+            raise exceptions.ValidationError(de)
---
+++ tests/test_contrib_pint_fields.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import unittest
+from odin.contrib.pint import units, FloatQField
+from odin.contrib.pint.units import registry
+from odin.exceptions import ValidationError
+
+
+class PintFieldsTestCase(unittest.TestCase):
+    def test_floatqfield_1(self):
+        f = FloatQField('kWh')
+        self.assertRaises(ValidationError, f.clean, None)
+        self.assertRaises(ValidationError, f.clean, 10.2 * registry.meter)
+        self.assertRaises(ValidationError, f.clean, 'abc')
+        self.assertEqual(10.2 * registry.kilowatt_hour, f.clean(10.2 * registry.kilowatt_hour))
+        self.assertEqual(10.2 * registry['kWh'], f.clean(10.2))
+        self.assertEqual(10.2 * registry['kWh'], f.clean((10.2, 'kWh')))
+        self.assertEqual(10.2 * registry['kWh'], f.clean((10.2, registry.kilowatt_hour)))
+        self.assertEqual(10.2 * registry.watt_hour, f.clean(10.2 * registry.watt_hour))
+
