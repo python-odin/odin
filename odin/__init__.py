05de7b7bc449811bae67e59ef102d7d04d3253cd
Tim Savage <tim.savage@poweredbypenguins.org>
2013-11-30 02:39:28 +1100

    Bumped version to 0.4.1

--- odin/__init__.py
+++ odin/__init__.py
@@ -1,7 +1,7 @@
 __authors__ = "Tim Savage"
 __author_email__ = "tim.savage@poweredbypenguins.org"
 __copyright__ = "Copyright (C) 2013 Tim Savage"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from odin.resources import Resource
 from odin.fields import *
