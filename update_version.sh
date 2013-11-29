62ad52e4fadc2a140b1f172c5ce79bed71c722bb
Tim Savage <tim.savage@poweredbypenguins.org>
2013-11-30 02:34:19 +1100

    Update version update script

--- update_version.sh
+++ update_version.sh
@@ -3,5 +3,5 @@
 
 sed -i "s/version=\"[.0-9]*\"/version=\"$1\"/g" setup.py
 sed -i "s/release = '[.0-9]*'/release = '$1'/g" doc/conf.py
-sed -i "s/__version__ = \"[.0-9]*\"/__version__ = \"$1\"/g" src/odin/__init__.py
+sed -i "s/__version__ = \"[.0-9]*\"/__version__ = \"$1\"/g" odin/__init__.py
 
