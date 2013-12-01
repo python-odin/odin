1f18497eb8990003396c0d520f6bbd119e8ca783
Tim Savage <tim.savage@poweredbypenguins.org>
2013-12-01 17:40:06 +1100

    Added new "framework" for serializers, serializers are used by codecs to serialize data into specific formats

---
+++ odin/serializers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import datetime
+from odin import datetimeutil
+
+
+class DatetimeEcmaFormat(object):
+    """
+    Serialize a datetime object into the ECMA defined format.
+    """
+    input_type = datetime.datetime
+
+    def __init__(self, assume_local=True):
+        self.assume_local = True
+
+    def __call__(self, value):
+        assert isinstance(value, self.input_type)
+        return datetimeutil.to_ecma_date_string(value, self.assume_local)
+
+datetime_ecma_format = DatetimeEcmaFormat()
+
