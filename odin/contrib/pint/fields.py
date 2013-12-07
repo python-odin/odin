84976c47bbdc2f64fa85383f17186cd76b2afe0b
Tim Savage <tim.savage@poweredbypenguins.org>
2013-12-07 14:03:54 +1100

    Split field to allow for simpler extension

--- odin/contrib/pint/fields.py
+++ odin/contrib/pint/fields.py
@@ -6,16 +6,12 @@ from odin.contrib.pint.units import registry
 from odin.fields import Field
 from odin.validators import EMPTY_VALUES
 
-__all__ = ('FloatQField',)
+__all__ = ('FloatField',)
 
 
-class FloatQField(Field):
-    default_error_messages = {
-        'invalid': "'%s' value must be a float.",
-    }
-
+class PintField(Field):
     def __init__(self, units, **kwargs):
-        super(FloatQField, self).__init__(**kwargs)
+        super(PintField, self).__init__(**kwargs)
 
         # Ensure we have valid units
         if units is None:
@@ -27,22 +23,41 @@ class FloatQField(Field):
         #                     "registered with Odin's unit registry.")
         self.units = units
 
-    def to_python(self, value):
-        if value in EMPTY_VALUES:
-            return None
+    def to_quantity(self, value):
         if not isinstance(value, registry.Quantity):
-            if isinstance(value, tuple) and len(value) == 2:
-                magnitude, units = value
+            # Split out units from underlying magnitudes
+            if isinstance(value, (tuple, list)) and len(value) == 2:
+                raw_value, units = value
             else:
-                magnitude, units = value, self.units
+                raw_value, units = value, self.units
+            # Extract the units
             if isinstance(units, six.string_types):
                 units = registry[units]
-            try:
-                value = float(magnitude) * units
-            except ValueError:
-                msg = self.error_messages['invalid'] % value
-                raise exceptions.ValidationError(msg)
+            # Convert the magnitude and apply units
+            value = self.to_magnitude(raw_value) * units
+
         try:
             return value.to(self.units)
         except DimensionalityError as de:
             raise exceptions.ValidationError(de)
+
+    def to_magnitude(self, value):
+        raise NotImplementedError()
+
+
+class FloatField(PintField):
+    default_error_messages = {
+        'invalid': "'%s' value must be a float.",
+    }
+
+    def to_magnitude(self, value):
+        try:
+            return float(value)
+        except ValueError:
+            msg = self.error_messages['invalid'] % value
+            raise exceptions.ValidationError(msg)
+
+    def to_python(self, value):
+        if value in EMPTY_VALUES:
+            return None
+        return self.to_quantity(value)
