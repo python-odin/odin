88f99c0c0cd80b0c567b6a6bf4350db140d78d41
Tim Savage <tim.savage@poweredbypenguins.org>
2013-12-10 02:12:55 +1100

    Initial work for building mappings.

--- odin/__init__.py
+++ odin/__init__.py
@@ -3,6 +3,7 @@ __author_email__ = "tim.savage@poweredbypenguins.org"
 __copyright__ = "Copyright (C) 2013 Tim Savage"
 __version__ = "0.4.1"
 
-from odin.resources import Resource
 from odin.fields import *
 from odin.fields.composite import *
+from odin.mapping import *
+from odin.resources import Resource
