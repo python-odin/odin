beda992cc2c11886086057ba5e025c8efceee3f6
Tim Savage <tim.savage@poweredbypenguins.org>
2014-01-16 00:19:46 +1100

    Added serialises for date, time and datetime to an ISO format

--- odin/serializers.py
+++ odin/serializers.py
@@ -3,6 +3,46 @@ import datetime
 from odin import datetimeutil
 
 
+def date_iso_format(value):
+    """
+    Serialise a datetime.date to ISO string format.
+    """
+    assert isinstance(value, datetime.date)
+    return value.isoformat()
+
+
+class TimeIsoFormat(object):
+    """
+    Serialise a datetime.time to ISO string format.
+    """
+    def __init__(self, default_timezone=datetimeutil.local):
+        self.default_timezone = default_timezone
+
+    def __call__(self, value):
+        assert isinstance(value, datetime.time)
+        if value.tzinfo() is None:
+            value = value.replace(tzinfo=self.default_timezone)
+        return value.isoformat()
+
+time_iso_format = TimeIsoFormat()
+
+
+class DatetimeIsoFormat(object):
+    """
+    Serialise a datetime.datetime to ISO string format.
+    """
+    def __init__(self, default_timezone=datetimeutil.local):
+        self.default_timezone = default_timezone
+
+    def __call__(self, value):
+        assert isinstance(value, datetime.datetime)
+        if value.tzinfo() is None:
+            value = value.replace(tzinfo=self.default_timezone)
+        return value.isoformat()
+
+datetime_iso_format = DatetimeIsoFormat()
+
+
 class DatetimeEcmaFormat(object):
     """
     Serialize a datetime object into the ECMA defined format.
