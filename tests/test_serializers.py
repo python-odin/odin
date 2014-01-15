f7f05906b2e3795cb0f1ca050db6c198c2384551
Tim Savage <tim.savage@poweredbypenguins.org>
2014-01-16 00:54:17 +1100

    Python 3 gotcha, numbers can't start with leading 0's

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -7,7 +7,7 @@ from odin import serializers
 
 class SerializerTestCase(unittest.TestCase):
     def test_date_iso_format(self):
-        self.assertEqual("2014-01-16", serializers.date_iso_format(datetime.date(2014, 01, 16)))
+        self.assertEqual("2014-01-16", serializers.date_iso_format(datetime.date(2014, 1, 16)))
 
     def test_time_iso_format(self):
         self.assertEqual(
@@ -32,37 +32,37 @@ class SerializerTestCase(unittest.TestCase):
         self.assertEqual(
             "2014-01-16T00:29:32+00:00",
             serializers.DatetimeIsoFormat(datetimeutil.utc)(
-                datetime.datetime(2014, 01, 16, 0, 29, 32)))
+                datetime.datetime(2014, 1, 16, 0, 29, 32)))
         self.assertEqual(
             "2014-01-16T00:29:32+10:00",
             serializers.DatetimeIsoFormat(datetimeutil.utc)(
-                datetime.datetime(2014, 01, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
+                datetime.datetime(2014, 1, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
 
         self.assertEqual(
             "2014-01-16T00:29:32-03:30",
             serializers.DatetimeIsoFormat(datetimeutil.FixedTimezone(-3, -30, 'TEST'))(
-                datetime.datetime(2014, 01, 16, 0, 29, 32)))
+                datetime.datetime(2014, 1, 16, 0, 29, 32)))
         self.assertEqual(
             "2014-01-16T00:29:32+10:00",
             serializers.DatetimeIsoFormat(datetimeutil.FixedTimezone(-3, -30, 'TEST'))(
-                datetime.datetime(2014, 01, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
+                datetime.datetime(2014, 1, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
 
     def test_datetime_ecma_format(self):
         self.assertEqual(
             "2014-01-16T00:29:32.000Z",
             serializers.DatetimeEcmaFormat(False, default_timezone=datetimeutil.utc)(
-                datetime.datetime(2014, 01, 16, 0, 29, 32)))
+                datetime.datetime(2014, 1, 16, 0, 29, 32)))
         self.assertEqual(
             "2014-01-15T14:29:32.000Z",
             serializers.DatetimeEcmaFormat(False, default_timezone=datetimeutil.utc)(
-                datetime.datetime(2014, 01, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
+                datetime.datetime(2014, 1, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
 
         self.assertEqual(
             "2014-01-16T03:59:32.000Z",
             serializers.DatetimeEcmaFormat(False, default_timezone=datetimeutil.FixedTimezone(-3, -30, 'TEST'))(
-                datetime.datetime(2014, 01, 16, 0, 29, 32)))
+                datetime.datetime(2014, 1, 16, 0, 29, 32)))
         self.assertEqual(
             "2014-01-15T14:29:32.000Z",
             serializers.DatetimeEcmaFormat(False, default_timezone=datetimeutil.FixedTimezone(-3, -30, 'TEST'))(
-                datetime.datetime(2014, 01, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
+                datetime.datetime(2014, 1, 16, 0, 29, 32, tzinfo=datetimeutil.FixedTimezone(10, 0, "10:00"))))
 
