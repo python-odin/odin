5901e6140a8d5ae7e8a4880fa6ca75f62af5a802
tims <tims@sakurasky.com>
2015-03-13 14:21:51 +1100

    Bumped version to 0.5.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='odin',
-    version='0.5.6',
+    version='0.5.7',
     url='https://github.com/timsavage/odin',
     license='LICENSE',
     author='Tim Savage',
