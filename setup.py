3007df3819f1d5d6522204810eb4944af782f005
tims <tims@sakurasky.com>
2015-09-02 14:30:30 +1000

    integrate bumpversion, role version back to 0.6.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='odin',
-    version='0.6.0.1',
+    version='0.6.0',
     url='https://github.com/timsavage/odin',
     license='LICENSE',
     author='Tim Savage',
