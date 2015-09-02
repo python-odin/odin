0cb6c61d7caa69fad47870061c426518597d819f
tims <tims@sakurasky.com>
2015-09-02 14:16:29 +1000

    Bumped to fix a release build issue

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='odin',
-    version='0.6',
+    version='0.6.0.1',
     url='https://github.com/timsavage/odin',
     license='LICENSE',
     author='Tim Savage',
