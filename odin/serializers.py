2c1096a62b3baa7d26f148a9787211446f79dce7
Tim Savage <tim.savage@poweredbypenguins.org>
2015-09-28 00:08:31 +1000

    Serialisation revisions, added registration for text serialisation.

--- odin/serializers.py
+++ odin/serializers.py
@@ -3,6 +3,21 @@ import datetime
 from odin import datetimeutil
 
 
+STRING_TYPES = {}
+
+
+def register_string_type(data_type, serialise_method):
+    """
+    Register a data type serialise method that results in a string.
+
+    :param data_type: a type instance that string data type should match.
+    :param serialise_method: a method that accepts a single value that will return a string representation of
+        the serialised type.
+
+    """
+    STRING_TYPES[data_type] = serialise_method
+
+
 def date_iso_format(value):
     """
     Serialise a datetime.date to ISO string format.
