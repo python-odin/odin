635b6da690d8724f8f367a7c4cc680c5c92bfaf2
Tim Savage <tim.savage@poweredbypenguins.org>
2015-09-28 02:33:19 +1000

    Added dump and load test

--- tests/test_codec_dict.py
+++ tests/test_codec_dict.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 import unittest
+import datetime
 from odin.codecs import dict_codec
+from odin.datetimeutil import utc
 from .resources import *
 
 
@@ -61,3 +63,27 @@ class DictCodecTestCase(unittest.TestCase):
                 'name': "Macmillan",
             },
         })
+
+    def test_dump_and_load_(self):
+        in_resource = Book(
+            title='Consider Phlebas',
+            num_pages=471,
+            rrp=19.50,
+            fiction=True,
+            genre="sci-fi",
+            authors=[Author(name="Iain M. Banks")],
+            publisher=Publisher(name="Macmillan"),
+            published=[datetime.datetime(1987, 1, 1, tzinfo=utc)]
+        )
+
+        d = dict_codec.dump(in_resource)
+        out_resource = dict_codec.load(d)
+
+        self.assertEqual(out_resource.title, in_resource.title)
+        self.assertEqual(out_resource.num_pages, in_resource.num_pages)
+        self.assertEqual(out_resource.rrp, in_resource.rrp)
+        self.assertEqual(out_resource.fiction, in_resource.fiction)
+        self.assertEqual(out_resource.genre, in_resource.genre)
+        self.assertEqual(out_resource.authors[0].name, in_resource.authors[0].name)
+        self.assertEqual(out_resource.publisher.name, in_resource.publisher.name)
+        self.assertEqual(out_resource.published[0], in_resource.published[0])
