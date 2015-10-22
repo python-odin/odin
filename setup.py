fed932260421d15e54f5bbddc5c57111046ff489
tims <tims@sakurasky.com>
2015-10-22 14:41:25 +1100

    Updated status to beta

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='odin',
-    version='0.7.0-alpha',
+    version='0.7.0-beta',
     url='https://github.com/timsavage/odin',
     license='LICENSE',
     author='Tim Savage',
