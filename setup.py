e7b3507f430f516759336ae835eb8a72f6d0ea31
tims <tims@sakurasky.com>
2015-11-04 18:18:43 +1100

    Bump version: 0.7b2 → 0.7b3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='odin',
-    version='0.7b2',
+    version='0.7b3',
     url='https://github.com/timsavage/odin',
     license='LICENSE',
     author='Tim Savage',
