33475e82041022e222d8c6ea698ffe0bab6d1e7a
Tim Savage <tim.savage@poweredbypenguins.org>
2015-11-18 23:55:17 +1100

    Added a pre-dispatch mixin that adds a pre-dispatch hook

--- odin/contrib/filter_query/__init__.py
+++ odin/contrib/filter_query/__init__.py
@@ -5,6 +5,8 @@ Query language for filtering::
     field == 42 AND field_2 == 'abc cde' OR field_3 = Null
 
 """
+from odin import filtering
+
 try:
     from ply import lex
 except ImportError:
@@ -20,3 +22,44 @@ tokens = (
     'LPAREN', 'RPAREN', 'NONE'
 )
 
+
+t_STRING = r'\".+\"'
+
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+
+
+
+FILTER_OPERATOR_MAP = {
+    "=": filtering.Equal, "==": filtering.Equal, "eq": filtering.Equal,
+    "!=": filtering.NotEqual, "<>": filtering.NotEqual, "neq": filtering.NotEqual,
+    "<":  filtering.LessThan, "lt": filtering.LessThan,
+    "<=": filtering.LessThanOrEqual, "lte": filtering.LessThanOrEqual,
+    ">":  filtering.GreaterThan, "gt": filtering.GreaterThan,
+    ">=": filtering.GreaterThanOrEqual, "gte": filtering.GreaterThanOrEqual,
+    "in": "In",
+    "is": "Is"
+}
+
+
+def _tokenize_string(query_str):
+    tokens = []
+    in_string = False
+    for c in query_str:
+        if c == ' ':
+            pass
+    return tokens
+
+
+def parse(query):
+    tokens = _tokenize_string(query)
