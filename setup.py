5d85bd5812e0d4106c68c08bb30893538c4d011f
Tim Savage <tim.savage@poweredbypenguins.org>
2016-02-26 16:15:21 +1100

    Bumped version to 0.7b4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='odin',
-    version='0.7b3',
+    version='0.7b4',
     url='https://github.com/timsavage/odin',
     license='LICENSE',
     author='Tim Savage',
