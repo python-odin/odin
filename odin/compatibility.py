b074a7e49419f803f92014fd5b81f3e3783c3f41
Tim Savage <tim.savage@poweredbypenguins.org>
2016-03-07 17:57:17 +1100

    Added tools for handling backwards compatibility etc

---
+++ odin/compatibility.py
@@ -0,0 +1,21 @@
+"""
+This module is to include utils for managing compatibility between Python and Odin releases.
+"""
+from six import wraps
+
+
+def deprecated(message):
+    """
+    Decorator for marking classes/functions as being deprecated and are to be removed in the future.
+
+    :param message: Message provided.
+
+    """
+    def inner(fun):
+        @wraps(fun)
+        def wrapper(*args, **kwargs):
+            import warnings
+            warnings.warn("{} is deprecated and scheduled for removal. {}".format(fun, message))
+            return fun(*args, **kwargs)
+        return wrapper
+    return inner
