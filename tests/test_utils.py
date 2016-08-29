292382c1bbb47077693c70bb9e65483baa07edb6
Tim Savage <tim@savage.company>
2016-08-29 19:33:59 +1000

    Added a test for chunk

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -33,12 +33,12 @@ class TestIterator(object):
         }
 
         assert {
-            'title': "My Title",
-            'num_pages': 1,
-            'rrp': 1.2,
-            'fiction': True,
-            'published': ['no_validation_for_type'],
-        } == utils.extract_fields_from_dict(source_dict, Book)
+                   'title': "My Title",
+                   'num_pages': 1,
+                   'rrp': 1.2,
+                   'fiction': True,
+                   'published': ['no_validation_for_type'],
+               } == utils.extract_fields_from_dict(source_dict, Book)
 
 
 class TestValueInChoice(object):
@@ -66,17 +66,41 @@ class TestForceTuple(object):
     """
     Tests for odin.utils.force_tuple
     """
+
     @pytest.mark.parametrize(('value', 'expected'), (
-        (None, tuple()),
-        ('', ('',)),
-        (0, (0,)),
-        (False, (False,)),
-        (True, (True,)),
-        (123, (123,)),
-        ('Foo', ('Foo',)),
-        (['Foo', 'bar'], ('Foo', 'bar')),
-        (('Foo', 'bar'), ('Foo', 'bar')),
-        (['Foo', 123, 'bar', 'eek'], ('Foo', 123, 'bar', 'eek')),
+            (None, tuple()),
+            ('', ('',)),
+            (0, (0,)),
+            (False, (False,)),
+            (True, (True,)),
+            (123, (123,)),
+            ('Foo', ('Foo',)),
+            (['Foo', 'bar'], ('Foo', 'bar')),
+            (('Foo', 'bar'), ('Foo', 'bar')),
+            (['Foo', 123, 'bar', 'eek'], ('Foo', 123, 'bar', 'eek')),
     ))
     def test_values(self, value, expected):
         assert utils.force_tuple(value) == expected
+
+
+class TestChunk(object):
+    """
+    Tests for odin.utils.chunk
+    """
+
+    @pytest.mark.parametrize(('value', 'size', 'expected'), (
+        ("This is a block of text split by 4", 4, [
+            ['T', 'h', 'i', 's'],
+            [' ', 'i', 's', ' '],
+            ['a', ' ', 'b', 'l'],
+            ['o', 'c', 'k', ' '],
+            ['o', 'f', ' ', 't'],
+            ['e', 'x', 't', ' '],
+            ['s', 'p', 'l', 'i'],
+            ['t', ' ', 'b', 'y'],
+            [' ', '4']
+        ]),
+    ))
+    def test_small_values(self, value, size, expected):
+        """Small scale test (eg small iterable that can be easily verified)"""
+        assert [list(c) for c in utils.chunk(value, size)] == expected
