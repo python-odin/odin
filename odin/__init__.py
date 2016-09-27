e7df7356caf384a2cfda080ed0d70157b4629667
Tim Savage <tim@savage.company>
2016-09-27 11:48:29 +1000

    Bump version: 0.10b4 → 0.10b5

--- odin/__init__.py
+++ odin/__init__.py
@@ -1,7 +1,7 @@
 __authors__ = "Tim Savage"
 __author_email__ = "tim@savage.company"
 __copyright__ = "Copyright (C) 2014 Tim Savage"
-__version__ = "0.10b4"
+__version__ = "0.10b5"
 
 from odin.fields import *  # noqa
 from odin.fields.composite import *  # noqa
