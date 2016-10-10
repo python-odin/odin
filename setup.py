b9146e36d37eae98576fd0c712078de12a29d493
Tim Savage <tim@savage.company>
2016-10-10 11:08:32 +1100

    Bump version: 0.10b8 → 0.10b9

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='odin',
 
-    version='0.10b8',
+    version='0.10b9',
 
     description='Data-structure definition/validation/traversal, mapping and serialisation toolkit for Python',
     long_description=long_description,
