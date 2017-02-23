5dfe27b853ef6dcc01b4e6a45026000020dd9e9f
Tim Savage <tim@savage.company>
2017-02-24 09:13:41 +1100

    Re-order so default handler is loaded before imports

--- odin/__init__.py
+++ odin/__init__.py
@@ -3,16 +3,16 @@ __author_email__ = "tim@savage.company"
 __copyright__ = "Copyright (C) 2014 Tim Savage"
 __version__ = "1.0b1"
 
-from odin.fields import *  # noqa
-from odin.fields.composite import *  # noqa
-from odin.fields.virtual import *  # noqa
-from odin.mapping import *  # noqa
-from odin.resources import Resource  # noqa
-from odin.adapters import ResourceAdapter  # noqa
-
 # Disable logging if an explicit handler is not added
 try:
     import logging
     logging.getLogger('odin').addHandler(logging.NullHandler())
 except AttributeError:
     pass  # Fallback for python 2.6
+
+from odin.fields import *  # noqa
+from odin.fields.composite import *  # noqa
+from odin.fields.virtual import *  # noqa
+from odin.mapping import *  # noqa
+from odin.resources import Resource  # noqa
+from odin.adapters import ResourceAdapter  # noqa
