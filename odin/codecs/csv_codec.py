ef66e9e8b4cb062fb24e348b3b6b30a771c59413
Tim Savage <tim@savage.company>
2017-03-02 16:31:00 +1100

    Changes to reader to make it easier to customise behaviour.
    The main approach used to is to make an iterable class (using the TypedResourceIterable interface), the big improvement is handing of validation errors, previously a validation error during processing would cause the iterator to raise a StopIteration even if the ValidationError was caught (the expected behaviour of yield) however this would mean that if you wanted to process an entire file and report bad rows.
    
    The only semi breaking change is dropping the *args param for passing additional params to the underlying csv.reader, I would strongly recommend using the **kwargs param anyway as it is more explicit.

--- odin/codecs/csv_codec.py
+++ odin/codecs/csv_codec.py
@@ -7,13 +7,182 @@ from odin.datastructures import CaseLessStringList
 from odin.fields import NOT_PROVIDED
 from odin.resources import create_resource_from_iter, create_resource_from_dict
 from odin.utils import getmeta, lazy_property
-from odin.exceptions import CodecDecodeError
+from odin.exceptions import CodecDecodeError, ValidationError
 
 CONTENT_TYPE = 'text/csv'
 
 
+class Reader(bases.TypedResourceIterable):
+    """
+    Customisable reader object.
+    """
+    csv_reader = csv.reader
+    """
+    CSV Reader object to use (if you wish to use *unicodecsv* or similar)
+    """
+
+    includes_header = True
+    """
+    File is expected to include a header.
+    """
+
+    ignore_header_case = False
+    """
+    Use case-less comparison on header fields.
+    """
+
+    strict_fields = False
+    """
+    Strictly check header fields.
+    """
+
+    csv_dialect = 'excel'
+    """
+    CSV Dialect to use; defaults to the CSV libraries default value of *excel*.
+    """
+
+    def __init__(self, f, resource_type, full_clean=True, error_callback=None, **reader_kwargs):
+        """
+        Initialise a reader
+
+        :param f: Input file (or file like) object to read
+        :param resource_type: Resource type to use as field template.
+        :param full_clean: Perform a full clean on objects
+        :param error_callback: Optional callback for errors
+        :param reader_kwargs: kwargs to pass to the csv_reader
+
+        """
+        super(Reader, self).__init__(resource_type)
+        self.full_clean = full_clean
+        if error_callback:
+            self.handle_validation_error = error_callback
+
+        # Backwards compatibility
+        for arg in ('csv_reader', 'includes_header', 'ignore_header_case', 'strict_fields', 'csv_dialect'):
+            if arg in reader_kwargs:
+                setattr(self, arg, reader_kwargs.pop(arg))
+
+        # Create reader instance
+        self._reader = self._create_reader(f, reader_kwargs)
+
+        # Configure header
+        if self.includes_header:
+            self.header = self._read_header()
+
+            # Handle strict fields
+            if self.strict_fields and self.extra_field_names:
+                raise CodecDecodeError("Extra unknown fields: {0}".format(','.join(self.extra_field_names)))
+
+        # Built in counters
+        self.row_count = None
+        self.error_count = None
+
+    def __iter__(self):
+        # Local vars
+        resource = self.resource_type
+        full_clean = self.full_clean
+        idx = -1
+
+        if self.includes_header:
+            mapping = self.field_mapping
+            error_count = 0
+
+            for idx, row in enumerate(self._reader):
+                # Check if row is less than mapping (as this will causes errors)!
+                try:
+                    yield create_resource_from_iter(
+                        (NOT_PROVIDED if s is None else row[s] for s in mapping), resource, full_clean
+                    )
+                except ValidationError as ve:
+                    # Don't raise these through yield as will cause a StopIteration
+                    # even if validation error can be handled safely.
+                    error_count += 1
+
+                    # Add one to index as row "0" will be the header
+                    self.handle_validation_error(ve, idx+1)
+
+            self.error_count = error_count
+
+        else:
+            for idx, row in enumerate(self._reader):
+                yield create_resource_from_iter(
+                    (NOT_PROVIDED if col is None else col for col in row), resource, full_clean
+                )
+
+        self.row_count = idx + 1  # Add one to get a count from the last index
+
+    def _create_reader(self, f, kwargs):
+        """
+        Create internal reader instance
+
+        :param f: File (or file like) object
+        :param kwargs: Dictionary of additional keyword args
+        :return: Reader instance
+
+        """
+        return self.csv_reader(f, self.csv_dialect, **kwargs)
+
+    def _read_header(self):
+        """
+        Get the header, this needs to be called **once** only!
+        """
+        header = next(self._reader)
+        if self.ignore_header_case:
+            header = CaseLessStringList(header)
+        return header
+
+    @lazy_property
+    def field_names(self):
+        """
+        Field names from resource.
+        """
+        fields = getmeta(self.resource_type).fields
+        if self.ignore_header_case:
+            return CaseLessStringList(field.name for field in fields)
+        else:
+            return tuple(field.name for field in fields)
+
+    @lazy_property
+    def extra_field_names(self):
+        """
+        Extra fields not included in header
+        """
+        return tuple(field for field in self.header if field not in self.field_names)
+
+    @lazy_property
+    def field_mapping(self):
+        """
+        Index mapping of CSV fields to resource fields.
+        """
+        mapping = []
+
+        # Add expected fields
+        header = self.header
+        for name in self.field_names:
+            if name in header:
+                mapping.append(header.index(name))
+            else:
+                mapping.append(None)
+
+        # Append any extra fields
+        for name in self.extra_field_names:
+            mapping.append(header.index(name))
+
+        return tuple(mapping)
+
+    def handle_validation_error(self, validation_error, row_index):
+        """
+        Method for handling of validation errors
+
+        :param validation_error: Validation error raised during processing.
+        :param row_index: Index number of the row.
+
+        """
+        raise validation_error
+
+
 def reader(f, resource, includes_header=False, csv_module=csv, full_clean=True,
-           ignore_header_case=False, strict_fields=False, *args, **kwargs):
+           ignore_header_case=False, strict_fields=False, **kwargs):
     """
     CSV reader that returns resource objects
 
@@ -28,94 +197,12 @@ def reader(f, resource, includes_header=False, csv_module=csv, full_clean=True,
     :return: Iterable reader object
 
     """
-    csv_reader = csv_module.reader(f, *args, **kwargs)
-    if includes_header:
-        fields = getmeta(resource).fields
-
-        # Pre-generate field mapping
-        header = next(csv_reader)
-
-        # Make header case-less
-        if ignore_header_case:
-            header = CaseLessStringList(header)
-            field_names = CaseLessStringList(field.name for field in fields)
-        else:
-            field_names = [field.name for field in fields]
-
-        # Check for extra fields
-        extra_fields = [field for field in header if field not in field_names]
-
-        # Handle strict fields
-        if strict_fields and extra_fields:
-                raise CodecDecodeError("Extra unknown fields: {0}".format(','.join(extra_fields)))
-
-        mapping = []
-        # Add expected fields
-        for field in fields:
-            if field.name in header:
-                mapping.append(header.index(field.name))
-            else:
-                mapping.append(None)
-
-        # Append any extra fields
-        for field in extra_fields:
-            mapping.append(header.index(field))
-
-        # Iterate CSV and process input
-        for row in csv_reader:
-            yield create_resource_from_iter(
-                (NOT_PROVIDED if s is None else row[s] for s in mapping), resource, full_clean
-            )
-
-    else:
-        # Iterate CSV and process input
-        for row in csv_reader:
-            yield create_resource_from_iter(
-                (NOT_PROVIDED if col is None else col for col in row), resource, full_clean
-            )
-
-
-class Reader(bases.TypedResourceIterable):
-    """
-    Customisable reader object.
-    """
-    csv_reader = csv.reader
-
-    includes_header = True
-    ignore_header_case = False
-    strict_fields = False
-
-    def __init__(self, f, resource_type, full_clean=True, **reader_kwargs):
-        """
-        Initialise a reader
-
-        :param f: Input file (or file like) object to read
-        :param resource_type: Resource type to use as field template.
-        :param full_clean: Perform a full clean on objects
-        :param reader_kwargs: kwargs to pass to the csv_reader
-
-        """
-        super(Reader, self).__init__(resource_type)
-        self.full_clean = full_clean
-
-        # Create reader instance
-        self._reader = self.csv_reader(f, **reader_kwargs)
-
-    def __iter__(self):
-        pass
-
-    @lazy_property
-    def header(self):
-        return None
-
-    def handle_validation_error(self, validation_error):
-        """
-        Method for handling of validation errors
-
-        :param validation_error:
-
-        """
-        raise validation_error
+    return Reader(f, resource, full_clean,
+                  csv_reader=csv_module.reader,
+                  includes_header=includes_header,
+                  ignore_header_case=ignore_header_case,
+                  strict_fields=strict_fields,
+                  **kwargs)
 
 
 class ResourceReader(csv.DictReader):
