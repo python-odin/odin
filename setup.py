0738b0f8e85dfaf9f6e140b556dc5d219f0e55c9
Tim Savage <tim@savage.company>
2017-03-03 09:01:14 +1100

    Bumped version to 1.0b2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='odin',
 
-    version='1.0b1',
+    version='1.0b2',
 
     description='Data-structure definition/validation/traversal, mapping and serialisation toolkit for Python',
     long_description=long_description,
