7f65a0c6783b91e3d6f5a419d8687c73b0fe2769
Tim Savage <tim@savage.company>
2017-09-07 00:43:11 +1000

    Added code to handle include/exclude of fields.

--- odin/proxy.py
+++ odin/proxy.py
@@ -1,9 +1,12 @@
 import six
 
+from odin.fields import NOT_PROVIDED
+from odin.utils import getmeta
+
 
 class ResourceProxyOptions(object):
     META_OPTION_NAMES = (
-        'resource', 'readonly', 'include', 'exclude',
+        'resource', 'include', 'exclude', 'readonly',
         # Overrides
         'name', 'namespace', 'name_space', 'verbose_name', 'verbose_name_plural', 'doc_group',
         'field_sorting',
@@ -11,10 +14,18 @@ class ResourceProxyOptions(object):
 
     def __init__(self, meta):
         self.meta = meta
-        self.parents = []
+        self.resource = None
         self.fields = []
-        self._key_fields = []
-        self.virtual_fields = []
+        self.include = []
+        self.exclude = []
+        self.readonly = []
+
+        self.name = None
+        self.name_space = NOT_PROVIDED
+        self.verbose_name = None
+        self.verbose_name_plural = None
+        self.doc_group = None
+        self.field_sorting = False
 
     def __repr__(self):
         return '<ProxyOptions for {}>'.format()
@@ -45,14 +56,25 @@ class ResourceProxyOptions(object):
                 raise TypeError("'class Meta' got invalid attribute(s): %s" % ','.join(meta_attrs.keys()))
         del self.meta
 
-        # Ensure resource is defined
-
 
 class ResourceProxyType(type):
     meta_options = ResourceProxyOptions
 
     def __new__(mcs, name, bases, attrs):
         super_new = super(ResourceProxyType, mcs).__new__
+
+        # attrs will never be empty for classes declared in the standard way
+        # (ie. with the `class` keyword). This is quite robust.
+        if name == 'NewBase' and attrs == {}:
+            return super_new(mcs, name, bases, attrs)
+
+        parents = [
+            b for b in bases if
+            isinstance(b, ResourceProxyType) and not (b.__name__ == 'NewBase' and b.__mro__ == (b, object))
+        ]
+        if not parents:
+            # If this isn't a subclass of Resource, don't do anything special.
+            return super_new(mcs, name, bases, attrs)
 
         # Create the class.
         module = attrs.pop('__module__')
@@ -67,10 +89,31 @@ class ResourceProxyType(type):
         new_meta = mcs.meta_options(meta)
         new_class.add_to_class('_meta', new_meta)
 
-        if not meta.resource:
+        if not new_meta.resource:
             raise AttributeError("'resource' meta option not defined.")
 
+        base_meta = getmeta(meta.resource)
+
+        # Use sets to determine items
+        fields = set(base_meta.field_map)
+        include = set(new_meta.include)
+        exclude = set(new_meta.exclude)
+        readonly = set(new_meta.readonly)
+
+        if include:
+            fields.intersection_update(include)
+        if exclude:
+            fields.difference_update(exclude)
+
+        new_meta.fields = fields
+
         return new_class
+
+    def add_to_class(cls, name, value):
+        if hasattr(value, 'contribute_to_class'):
+            value.contribute_to_class(cls, name)
+        else:
+            setattr(cls, name, value)
 
 
 class ResourceProxyBase(object):
@@ -85,4 +128,21 @@ class ResourceProxyBase(object):
 
 
 class ResourceProxy(six.with_metaclass(ResourceProxyType, ResourceProxyBase)):
+    pass
+
+
+if __name__ == '__main__':
+    import odin
+
+
+    class Sample(odin.Resource):
+        name = odin.StringField()
+        year = odin.IntegerField()
+
+
+    class SampleProxy(ResourceProxy):
+        class Meta:
+            resource = Sample
+            exclude = ['name']
+
     pass
