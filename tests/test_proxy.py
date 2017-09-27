af2aaf2698243d6604b63113f851be3c1d13e7bf
Tim Savage <tim@savage.company>
2017-09-27 19:20:13 +1000

    More test cases for ResourceProxy

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,9 +1,8 @@
 import pytest
+
 from odin.utils import getmeta
-
-import odin
-
 from odin import proxy
+
 from .resources import Book
 
 
@@ -64,3 +63,74 @@ class TestFilterFields(object):
 
         assert fields == expected_fields
         assert readonly == expected_readonly
+
+
+class BookProxy(proxy.ResourceProxy):
+    class Meta:
+        resource = Book
+        include = ('title', 'isbn', 'num_pages', 'rrp')
+        verbose_name = 'Book Summary'
+        namespace = 'the.other.library'
+
+    @property
+    def expensive(self):
+        return self.rrp > 200
+
+
+class TestResourceProxyMeta(object):
+    def test_no_meta(self):
+        with pytest.raises(AttributeError):
+            class SampleProxy(proxy.ResourceProxy):
+                pass
+
+    def test_no_resource(self):
+        with pytest.raises(AttributeError):
+            class SampleProxy(proxy.ResourceProxy):
+                class Meta:
+                    pass
+
+
+class TestResourceProxyOptions(object):
+    def test_options_type(self):
+        actual = getmeta(BookProxy)
+        assert isinstance(actual, proxy.ResourceProxyOptions)
+
+    def test_invalid_option(self):
+        with pytest.raises(TypeError):
+            class SampleProxy(proxy.ResourceProxy):
+                class Meta:
+                    resource = Book
+                    eek = 'boo!'
+
+    @pytest.mark.parametrize('attr, actual', (
+        ('key_field_name', 'isbn'),
+        ('verbose_name', 'Book Summary'),
+        ('verbose_name_plural', 'Book Summarys'),
+        ('include', ('title', 'isbn', 'num_pages', 'rrp')),
+        ('name_space', 'the.other.library'),
+    ))
+    def test_options(self, attr, actual):
+        target = getmeta(BookProxy)
+
+        assert actual == getattr(target, attr)
+
+
+class TestResourceProxy(object):
+    def test_shadow(self):
+        target = BookProxy()
+        actual = target.get_shadow()
+
+        assert isinstance(actual, Book)
+
+    def test_proxy(self):
+        actual = Book(title='1984')
+        target = BookProxy.proxy(actual)
+
+        assert target.title == '1984'
+        assert actual is target.get_shadow()
+
+    def test_extra_property(self):
+        book = Book(rrp=256)
+        target = BookProxy.proxy(book)
+
+        assert target.expensive
