5831648dbbfbef35a08bd204e3feeddc93e522b3
Tim Savage <tim@savage.company>
2021-05-26 01:01:19 +1000

    Update sample new resource

--- tests/test_new_resources.py
+++ tests/test_new_resources.py
@@ -1,6 +1,9 @@
+from datetime import date
 from enum import Enum
+from typing import List, Optional
 
 from odin import new_resources
+from .resources import Author, Publisher
 
 
 class Genre(Enum):
@@ -21,6 +24,6 @@ class Book(new_resources.NewResource):
     rrp: float = 20.4
     fiction: bool
     genre: Genre
-    published: List[datetime]
+    published: List[date]
     authors: List[Author]
     publisher: Optional[Publisher] = None
