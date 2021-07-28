cce1752cbd9d30fee8b88f29ffe2d4b542e22679
Tim Savage <tim@savage.company>
2021-07-28 23:44:25 +1000

    Update placeholder test cases to not break on python < 3.6

---
+++ tests/new_resources.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import enum
+import uuid
+from datetime import datetime
+from typing import List, Optional
+
+from odin.new_resource import NewResource, AbstractResource, Options
+
+
+class Author(NewResource):
+    name: str
+
+    class Meta:
+        name_space = None
+
+
+class Publisher(NewResource):
+    name: str
+
+    class Meta:
+        name_space = None
+
+
+class LibraryBook(AbstractResource):
+    class Meta:
+        abstract = True
+        name_space = "new_library"
+
+
+class Book(LibraryBook):
+    class Meta:
+        key_field_name = "isbn"
+
+    title: str
+    isbn: str
+    num_pages: int
+    rrp: float = Options(20.4, use_default_if_not_provided=True)
+    fiction: bool = Options(is_attribute=True)
+    genre: str = Options(
+        choices=(
+            ("sci-fi", "Science Fiction"),
+            ("fantasy", "Fantasy"),
+            ("biography", "Biography"),
+            ("others", "Others"),
+            ("computers-and-tech", "Computers & technology"),
+        )
+    )
+    published: List[datetime]
+    authors: List[Author] = Options(use_container=True)
+    publisher: Optional[Publisher]
+
+    def __eq__(self, other):
+        if other:
+            return vars(self) == vars(other)
+        return False
+
+
+class From(enum.Enum):
+    Dumpster = "dumpster"
+    Shop = "shop"
+    Ebay = "ebay"
+
+
+class IdentifiableBook(Book):
+    id: uuid.UUID
+    purchased_from: From
--- tests/test_new_resources.py
+++ tests/test_new_resources.py
@@ -1,49 +1,24 @@
-from datetime import date
-from enum import Enum
-from typing import List, Optional
+import sys
+import pytest
 
-from odin import new_resource
 from odin.utils import getmeta
-from .resources import Author, Publisher
+
+# Workaround to ensure testsuite works on Python < 3.6
+try:
+    import odin.new_resource as new_resource
+except SyntaxError:
+    if sys.version_info < (3, 6):
+        new_resource = None
+    else:
+        raise
+else:
+    from tests.new_resources import Book
 
 
-class Genre(Enum):
-    ScienceFiction = "sci-fi"
-    Fantasy = "fantasy"
-    Biography = "biography"
-    Others = "others"
-    ComputersAndTechnology = "computers-and-tech"
+@pytest.mark.skipif(new_resource is None, reason="Requires Python 3.6 or higher")
+class TestOptions:
+    def test_meta(self):
 
+        meta = getmeta(Book)
 
-class BaseBook(new_resource.AbstractResource):
-    title: str
-
-
-class OldBook(new_resource.NewResource):
-    title: str
-    isbn: str
-
-
-class NewBook(BaseBook):
-    class Meta:
-        key_field_name = "isbn"
-
-    isbn: str
-    num_pages: int
-    rrp: float = new_resource.Options(20.4, use_default_if_not_provided=True)
-    fiction: bool = new_resource.Options(is_attribute=True)
-    genre: Genre = Genre.ScienceFiction
-    published: List[date]
-    authors: List[Author]
-    publisher: Optional[Publisher]
-
-    def __eq__(self, other):
-        if other:
-            return vars(self) == vars(other)
-        return False
-
-
-def test_meta():
-    meta = getmeta(NewBook)
-
-    assert meta.fields == []
+        assert meta.fields == []
