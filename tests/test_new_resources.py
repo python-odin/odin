933780d550271950b8d979899a7c6eae4bfc8e87
Tim Savage <tim@savage.company>
2021-08-04 10:55:35 +1000

    Simplify test skipping

--- tests/test_new_resources.py
+++ tests/test_new_resources.py
@@ -5,17 +5,14 @@ from odin.utils import getmeta
 
 # Workaround to ensure testsuite works on Python < 3.6
 try:
-    import odin.new_resource as new_resource
+    from tests.new_resources import Book
 except SyntaxError:
     if sys.version_info < (3, 6):
-        new_resource = None
+        pytest.skip("Requires Python 3.6 or higher", allow_module_level=True)
     else:
         raise
-else:
-    from tests.new_resources import Book
 
 
-@pytest.mark.skipif(new_resource is None, reason="Requires Python 3.6 or higher")
 class TestNewResourceType:
     def test_fields_are_identified_in_correct_order(self):
         meta = getmeta(Book)
