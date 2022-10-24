a426e9f8779c1f54365d5af0475b3f2dc5e299d6
Tim Savage <tim@savage.company>
2022-10-25 10:20:31 +1100

    Added python 3.11 into tests

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from nox.sessions import Session
 
 
-@nox.session(python=("3.8", "3.9", "3.10", "3.11b2"))
+@nox.session(python=("3.8", "3.9", "3.10", "3.11"))
 def tests(session: Session):
     # fmt: off
     session.run(
