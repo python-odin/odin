667ffde1d79df79448ff2646a8b9ce8a0fcfcc2d
Tim Savage <tim@savage.company>
2023-02-27 00:42:39 +1100

    Update validation tree with themes

---
+++ src/odin/contrib/rich/theme.py
@@ -0,0 +1,22 @@
+"""Rich Theme definition."""
+from typing import Dict
+
+from rich import get_console
+from rich.theme import Theme
+from rich.style import Style
+
+ODIN_STYLES: Dict[str, Style] = {
+    "odin.resource.name": Style(color="bright_cyan"),
+    "odin.resource.error": Style(color="red", underline=True),
+    "odin.field.name": Style(color="bright_blue"),
+    "odin.field.error": Style(color="red", italic=True),
+    "odin.field.type": Style(color="magenta"),
+    "odin.field.doc": Style(),
+}
+
+odin_theme = Theme(ODIN_STYLES, inherit=False)
+
+
+def add_odin_theme():
+    """Add odin to builtin theme."""
+    get_console().push_theme(odin_theme)
